ab0693af70f47e4f1fa78ffeaa8baae8485ce86c
mustiyoruk <mustiyoruk@yahoo.com>
2023-07-06 10:58:38 +0300

    redux project is enhanced with react-redux library

---
+++ src/features/allRecipes/AllRecipes.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+// Add useDispatch to the import statement below.
+import { useSelector, useDispatch } from "react-redux";
+
+import { addRecipe } from "../favoriteRecipes/favoriteRecipesSlice.js";
+import { loadData, selectFilteredAllRecipes } from "./allRecipesSlice.js";
+import FavoriteButton from "../../components/FavoriteButton";
+import Recipe from "../../components/Recipe";
+
+const favoriteIconURL =
+  "https://static-assets.codecademy.com/Courses/Learn-Redux/Recipes-App/icons/favorite.svg";
+
+export const AllRecipes = () => {
+  const allRecipes = useSelector(selectFilteredAllRecipes);
+  // Implement dispatch variable below.
+  const dispatch = useDispatch();
+
+  const onFirstRender = () => {
+    dispatch(loadData());
+  };
+  useEffect(onFirstRender, [dispatch]);
+
+  const onAddRecipeHandler = (recipe) => {
+    dispatch(addRecipe(recipe));
+  };
+
+  return (
+    <div className="recipes-container">
+      {allRecipes.map((recipe) => (
+        <Recipe recipe={recipe} key={recipe.id}>
+          <FavoriteButton
+            onClickHandler={() => onAddRecipeHandler(recipe)}
+            icon={favoriteIconURL}
+          >
+            Add to Favorites
+          </FavoriteButton>
+        </Recipe>
+      ))}
+    </div>
+  );
+};
